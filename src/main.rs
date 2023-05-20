019011958bea0a150f5c731fdee460da49cc49de
Gen4ralz <76762117+Gen4ralz@users.noreply.github.com>
2023-05-20 15:24:56 +0700

    Add First Endpoint: Basic Health Check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer, Responder, HttpResponse};
+
+async fn greet() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+        HttpServer::new(|| {
+            App::new()
+                .route("/", web::get().to(greet))
+                .route("/health_check", web::get().to(health_check))
+        })
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
 }
